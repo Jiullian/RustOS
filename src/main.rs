01b9d1ccbe6a83aee3bc5dc8b72de43b11db84f6
jiullian <jiullian77470@gmail.com>
2026-02-10 21:31:27 +0100

    feat - start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Ne pas utiliser les bibliothèques standard de Rust
+#![no_main] // Désactiver les points d'entrées standard de Rust
+
+use core::panic::PanicInfo;
+
+// Fonction point d'entrée du système.
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+// Fonction appelée lors de chaque panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
