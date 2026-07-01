7762433743cc5f36592a180f9a0667e7174fc003
Théo <theo.berquer05@gmail.com>
2026-07-01 10:02:58 +0200

    delete - demo/test line

--- src/main.rs
+++ src/main.rs
@@ -5,71 +5,33 @@
 #![test_runner(RustOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use RustOS::println;
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use x86_64::structures::paging::PageTable;
+use RustOS::println;
 extern crate alloc;
-
-use alloc::boxed::Box;
-use alloc::vec::Vec;
-use alloc::vec;
-use alloc::rc::Rc;
-
-// Création d'une variable static
-// b"Hello World!" => b pour créer une chaîne de caractères d'octets. VGA ne comprend que l'ASCII ET non l'UNICODE
-static HELLO: &[u8] = b"Hello World!";
 
 entry_point!(kernel_main);
 
 // Fonction point d'entrée du système.
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use RustOS::allocator;
-    use RustOS::memory::{self,BootInfoFrameAllocator};
-    use x86_64::{
-        VirtAddr,
-        structures::paging::{Page, Translate},
-    };
+    use RustOS::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
 
-    println!("Hello World{}", "!");
+    // Initialisation du système (GDT, IDT, interrupts, disque, titre)
     RustOS::init();
 
+    // Initialisation de la mémoire dynamique (heap)
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator =
         unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
-
-    // allouer un nombre dans la heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // créer un vecteur à taille dynamique
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
-
-    // pointage factice d'une page virtuelle libre (très très éloignée)
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    //  on écrit `New!` (en little endian) et on l'affiche
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
 
-    println!("No crash! ;)");
+    // Boucle d'attente du CPU (attend les interruptions clavier)
     RustOS::hlt_loop();
 }
 
